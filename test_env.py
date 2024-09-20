cc14d59d161172fcdfcdb0f8ccc5c5a85d9589ff
Adlil Yaacob <adlil.yaacob@gmail.com>
2024-09-20 07:37:11 +0000

    clean up

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-print(f"FLASK_EXPOSE_HOST: {os.getenv('FLASK_EXPOSE_HOST')}")
-print(f"FLASK_EXPOSE_PORT: {os.getenv('FLASK_EXPOSE_PORT')}")
